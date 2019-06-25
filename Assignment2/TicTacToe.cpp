a6250e276525c152fbe1df7c2679a004e9fd87e3
kevin20000k <30421671+kevin20000k@users.noreply.github.com>
2019-06-24 23:44:39 -0700

    Update and rename x.cpp to TicTacToe.cpp

---
+++ Assignment2/TicTacToe.cpp
@@ -0,0 +1,80 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Kevin Zhou
+ *
+ * Created on June 24, 2019, 11:05 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+bool isWon(char, char[][3]);
+bool isDraw(char[][3]);
+void displayBoard(char[][3]);
+void makeAMove(char[][3], char);
+
+int main() {
+	//
+	//	PLEASE DO NOT CHANGE function main
+	//
+	char board[3][3] = { { ' ', ' ', ' ' },{ ' ', ' ', ' ' },{ ' ', ' ', ' ' } };
+	displayBoard(board);
+
+	while (true) {
+		// The first player makes a move
+		makeAMove(board, 'X');
+		displayBoard(board);
+		if (isWon('X', board)) {
+			cout << "X player won" << endl;
+			exit(0);
+		}
+		else if (isDraw(board)) {
+			cout << "No winner" << endl;
+			exit(0);
+		}
+
+		// The second player makes a move
+		makeAMove(board, 'O');
+		displayBoard(board);
+
+		if (isWon('O', board)) {
+			cout << "O player won" << endl;
+			exit(0);
+		}
+		else if (isDraw(board)) {
+			cout << "No winner" << endl;
+			exit(0);
+		}
+	}
+
+	return 0;
+}
+
+bool isWon(char num1, char num2[][3]) {
+    
+    return false;
+}
+
+bool isDraw(char num1[][3]) {
+    
+    return false;
+}
+
+void makeAMove(char num1[][3], char num2) {
+    
+    
+}
+
+void displayBoard(char num1[][3]) {
+    
+    
+}
+
+
