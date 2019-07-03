5f39707a54e5b280b1d166257783fe5f18d5e47e
kevin20000k <30421671+kevin20000k@users.noreply.github.com>
2019-07-03 16:44:40 -0700

    Update TicTacToe.cpp

--- Assignment2/TicTacToe.cpp
+++ Assignment2/TicTacToe.cpp
@@ -1,11 +1,4 @@
 
-/* 
- * File:   main.cpp
- * Author: Kevin Zhou
- *
- * Created on June 24, 2019, 11:05 PM
- */
-
 #include <iostream>
 using namespace std;
 
@@ -14,9 +7,9 @@ bool isDraw(char[][3]);
 void displayBoard(char[][3]);
 void makeAMove(char[][3], char);
 
-bool checkHorizontal(char, char[][3]);
-bool checkVertical(char, char[][3]);
-bool checkDiagonal(char, char[][3]);
+bool tempCheckHorizontal(char, char[][3]);
+bool tempCheckVertical(char, char[][3]);
+bool tempCheckDiagonal(char, char[][3]);
 
 int main() {
 	//
@@ -57,11 +50,12 @@ int main() {
 
 bool isWon(char tempPlayer, char tempBoard[][3]) {
     
-    bool h = checkHorizontal(tempPlayer, tempBoard);
-    bool v = checkVertical(tempPlayer, tempBoard);
-    bool d = checkDiagonal(tempPlayer, tempBoard);
+    bool tempH = tempCheckHorizontal(tempPlayer, tempBoard);
+    bool tempV = tempCheckVertical(tempPlayer, tempBoard);
+    bool tempD = tempCheckDiagonal(tempPlayer, tempBoard);
     
-    if(h || v || d) 
+    //If the user has won through either of these, its a win
+    if(tempH || tempV || tempD) 
         return true;
     else
         return false;
@@ -69,6 +63,7 @@ bool isWon(char tempPlayer, char tempBoard[][3]) {
 
 bool isDraw(char tempBoard[][3]) {
     
+    //Draw will happen if all spots are taken, meaning none are empty
     if(tempBoard[0][0] != ' ' && tempBoard[0][1] != ' ' && tempBoard[0][2] != ' ' && tempBoard[1][0] != ' ' && tempBoard[1][1] != ' ' && tempBoard[0][2] != ' ' 
             && tempBoard[2][0] != ' ' && tempBoard[2][1] != ' ' && tempBoard[2][2] != ' ') {
         return true;
@@ -78,8 +73,9 @@ bool isDraw(char tempBoard[][3]) {
     }
 }
 
-bool checkHorizontal(char tempPlayer, char tempBoard[][3]) {
+bool tempCheckHorizontal(char tempPlayer, char tempBoard[][3]) {
     
+    //Check all horizontal spots and see if there's 3 in a row for horizontal
             if(tempBoard[0][0] == tempPlayer && tempBoard[0][1] == tempPlayer && tempBoard[0][2] == tempPlayer) {
                 
                 return true;
@@ -97,8 +93,9 @@ bool checkHorizontal(char tempPlayer, char tempBoard[][3]) {
             }
 }
 
-bool checkVertical(char tempPlayer, char tempBoard[][3]) {
+bool tempCheckVertical(char tempPlayer, char tempBoard[][3]) {
             
+    //Check all vertical spots and see if there's 3 in a row for vertical
             if(tempBoard[0][0] == tempPlayer && tempBoard[1][0] == tempPlayer && tempBoard[2][0] == tempPlayer) {
                 
                 return true;
@@ -116,8 +113,9 @@ bool checkVertical(char tempPlayer, char tempBoard[][3]) {
             }
 }
 
-bool checkDiagonal(char tempPlayer, char tempBoard[][3]) {
+bool tempCheckDiagonal(char tempPlayer, char tempBoard[][3]) {
            
+    //Check all diagonal spots and see if there's 3 in a row for diagonal
             if(tempBoard[0][0] == tempPlayer && tempBoard[1][1] == tempPlayer && tempBoard[2][2] == tempPlayer) {
                 
                 return true;
@@ -138,6 +136,7 @@ void makeAMove(char tempBoard[][3], char tempPlayer) {
     
     bool tempCurrentTurnComplete = false;
     
+    //Ask the user to pick a spot
     cout << "Enter a row (0, 1, 2) for player " << tempPlayer << "   :  ";
     cin >> tempRowPosition;
     cout << "Enter a column (0, 1, 2) for player " << tempPlayer << "   :  ";
@@ -148,11 +147,11 @@ void makeAMove(char tempBoard[][3], char tempPlayer) {
             for(int j = 0; j < 3; j++) {
 
                 //If there is a spot available and matches specific desired spot, place it
-
                 if(i == tempRowPosition && j == tempColPosition && tempBoard[i][j] == ' ') { 
                     tempBoard[i][j] = tempPlayer;
                     tempCurrentTurnComplete = true;
                 }
+                //If spot is already occupied, meaning not available, change to false and not allow the next player to continue
                 else if(i == tempRowPosition && j == tempColPosition && tempBoard[i][j] != ' ') { 
                     cout << "This cell is already occupied. Try a different cell" << endl;
                     tempCurrentTurnComplete = false;
@@ -160,6 +159,8 @@ void makeAMove(char tempBoard[][3], char tempPlayer) {
             }
         }
     
+    //If the current player hasn't placed anything or is placing at wrong spot will tell user to pick another spot,
+    //Will run again, until the user has placed in available spot
     if(tempCurrentTurnComplete == false) {
        makeAMove(tempBoard, tempPlayer);
     }
