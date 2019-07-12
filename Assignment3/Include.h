f6b57e549dc3fcb20f40068fac97a1fcb1d3ba97
kevin20000k <30421671+kevin20000k@users.noreply.github.com>
2019-07-11 23:44:42 -0700

    Update Include.h

--- Assignment3/Include.h
+++ Assignment3/Include.h
@@ -1,5 +1,12 @@
 
 //	Include.h
+#include <iostream>
+#include <string>
+#include <vector>
+#include "LinkedBag.cpp"
+#include "LinkedBag340.cpp"
+//#include "LinkedBag.h"
+//#include "Node.h"
 
 
 using namespace std;
